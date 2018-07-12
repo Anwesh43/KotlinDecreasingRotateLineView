afb7fa79ea4fe59dd878a0f8f7fd957cba60ee10
Anwesh43 <anweshthecool0@gmail.com>
2018-07-13 01:36:04 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/kotlindecreasinglinerotateview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlindecreasinglinerotateview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.anwesh.uiprojects.kotlindecreasinglinerotateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.anwesh.uiprojects.decreasinglinerotateview.DecreasingLineRotateView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DecreasingLineRotateView.create(this)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
