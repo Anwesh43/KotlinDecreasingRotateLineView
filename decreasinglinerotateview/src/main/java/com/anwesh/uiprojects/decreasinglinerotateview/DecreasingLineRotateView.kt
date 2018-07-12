5be8f00d9b7cf999d74b561ad1572cd594f49f88
Anwesh43 <anweshthecool0@gmail.com>
2018-07-13 00:59:42 +0530

    adding DecreasingLineRotateView

--- decreasinglinerotateview/src/main/java/com/anwesh/uiprojects/decreasinglinerotateview/DecreasingLineRotateView.kt
+++ decreasinglinerotateview/src/main/java/com/anwesh/uiprojects/decreasinglinerotateview/DecreasingLineRotateView.kt
@@ -3,3 +3,24 @@ package com.anwesh.uiprojects.decreasinglinerotateview
 /**
  * Created by anweshmishra on 13/07/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val NODES : Int = 5
+
+class DecreasingLineRotateView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        return true
+    }
+}
