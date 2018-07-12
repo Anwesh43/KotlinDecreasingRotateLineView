91987729c0cf9d2f7b100879f638986991e60982
Anwesh43 <anweshthecool0@gmail.com>
2018-07-13 01:29:56 +0530

    adding DecreasingLineRotateView in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlindecreasinglinerotateview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlindecreasinglinerotateview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlindecreasinglinerotateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.decreasinglinerotateview.DecreasingLineRotateView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DecreasingLineRotateView.create(this)
     }
 }
